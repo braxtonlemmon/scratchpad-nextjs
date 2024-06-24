50125dac87e726c634e10c95b61fd98741d8b287
braxtonlemmon <braxtonlemmon@gmail.com>
2024-06-24 16:16:56 -0600

    add custom Main styled component and global body css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+import styled from 'styled-components'
+
+const Main = styled.main`
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
 export default function Home() {
-  return <main></main>
+  return <Main></Main>
 }
